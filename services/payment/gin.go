8b9d30ddda9601217ab05a1318c47a4e6b132799
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-08-19 05:54:13 +0800

    fix: use POST

--- services/payment/gin.go
+++ services/payment/gin.go
@@ -6,6 +6,6 @@ func (s *Service) Mount(r gin.IRouter) {
 	my := r.Group("/")
 	my.Use(s.authManager.Middleware(s.emailJwtAuth))
 
-	my.GET("/send", s.send())
-	my.GET("/withdraw", s.withdraw())
+	my.POST("/send", s.send())
+	my.POST("/withdraw", s.withdraw())
 }
