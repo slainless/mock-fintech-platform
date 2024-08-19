7a358363dcbfa5aeed473f1aebf2f8a656e1553d
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-08-19 11:00:18 +0800

    added mock payment service

---
+++ internal/util/rand.go
@@ -0,0 +1,14 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+func MockSleep(dur time.Duration) {
+	time.Sleep(time.Duration(rand.Int63n(int64(dur))))
+}
+
+func LeaveItToRNG() bool {
+	return rand.Intn(2) == 1
+}
---
+++ pkg/payment_service/mock_payment.go
@@ -0,0 +1,105 @@
+package payment_service
+
+import (
+	"context"
+	"errors"
+	"math/rand"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/slainless/mock-fintech-platform/internal/util"
+	"github.com/slainless/mock-fintech-platform/pkg/internal/artifact/database/mock_fintech/public/model"
+	"github.com/slainless/mock-fintech-platform/pkg/platform"
+)
+
+type MockPaymentService struct{}
+
+// Balance implements platform.PaymentService.
+func (s *MockPaymentService) Balance(ctx context.Context, account *platform.PaymentAccount) (*platform.MonetaryAmount, error) {
+	util.MockSleep(2 * time.Second)
+	return &platform.MonetaryAmount{
+		Value:    rand.Int63(),
+		Currency: "USD",
+	}, nil
+}
+
+// GetMatchingHistory implements platform.PaymentService.
+func (*MockPaymentService) GetMatchingHistory(ctx context.Context, account *platform.PaymentAccount, history *platform.TransactionHistory) (*platform.TransactionHistory, error) {
+	util.MockSleep(2 * time.Second)
+	if util.LeaveItToRNG() {
+		return nil, errors.New("Oops! Failed to get matching history")
+	} else {
+		note := "Received from " + account.UserUUID
+		match := *history
+		match.TransactionNote = &note
+		match.AccountUUID = *history.DestUUID
+		match.Mutation = history.Mutation * -1
+		match.Address = nil
+		return &match, nil
+	}
+}
+
+// Send implements platform.PaymentService.
+func (*MockPaymentService) Send(ctx context.Context, source *platform.PaymentAccount, des *platform.PaymentAccount, amount int64) (*platform.TransactionHistory, error) {
+	util.MockSleep(3 * time.Second)
+	if util.LeaveItToRNG() {
+		return nil, errors.New("Failed to send money")
+	} else {
+		uuid, err := uuid.NewV7()
+		if err != nil {
+			return nil, err
+		}
+
+		return &platform.TransactionHistory{
+			TransactionHistories: model.TransactionHistories{
+				UUID:            uuid.String(),
+				AccountUUID:     source.UUID,
+				DestUUID:        &des.PaymentAccounts.UserUUID,
+				Mutation:        amount,
+				Currency:        "USD",
+				TransactionDate: time.Now(),
+			},
+			ServiceUUID: source.ServiceID,
+			UserUUID:    source.UserUUID,
+		}, nil
+	}
+}
+
+// Validate implements platform.PaymentService.
+func (*MockPaymentService) Validate(ctx context.Context, user *platform.User, accountForeignID string, callbackData string) error {
+	util.MockSleep(2 * time.Second)
+	if util.LeaveItToRNG() {
+		return errors.New("Failed to validate user")
+	} else {
+		return nil
+	}
+}
+
+// Withdraw implements platform.PaymentService.
+func (*MockPaymentService) Withdraw(ctx context.Context, account *platform.PaymentAccount, amount int64, callbackData string) (*platform.TransactionHistory, error) {
+	util.MockSleep(2 * time.Second)
+	if util.LeaveItToRNG() {
+		return nil, errors.New("Failed to send money")
+	} else {
+		uuid, err := uuid.NewV7()
+		if err != nil {
+			return nil, err
+		}
+
+		return &platform.TransactionHistory{
+			TransactionHistories: model.TransactionHistories{
+				UUID:            uuid.String(),
+				AccountUUID:     account.UUID,
+				Mutation:        amount,
+				Currency:        "USD",
+				TransactionDate: time.Now(),
+			},
+			ServiceUUID: account.ServiceID,
+			UserUUID:    account.UserUUID,
+		}, nil
+	}
+}
+
+func NewMockPaymentService() platform.PaymentService {
+	return &MockPaymentService{}
+}
