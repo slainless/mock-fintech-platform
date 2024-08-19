616af4ab2fd1ddd3dee809c1671ba8134c39e463
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-08-20 05:48:59 +0800

    remove my prefix in user API

--- services/user/gin.go
+++ services/user/gin.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func (s *Service) Mount(r gin.IRouter) {
 	r.POST("/register", s.registerWithEmail())
 
-	my := r.Group("/my")
+	my := r.Group("/")
 	my.Use(s.authManager.Middleware(s.emailJwtAuth))
 	my.GET("/account", s.accounts())
 	my.GET("/history", s.histories())
