2980fcf9335fde3b44035e2e76c0c2e71ac107ad
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-08-20 10:21:29 +0800

    mig(4)!: added charging_method to recurring_payments

--- db/migrations/000004_alter_recurring_payments_add_column_and_constraint.down.sql
+++ db/migrations/000004_alter_recurring_payments_add_column_and_constraint.down.sql
@@ -1,4 +1,5 @@
 BEGIN;
 ALTER TABLE "recurring_payments" DROP COLUMN "foreign_id";
+ALTER TABLE "recurring_payments" DROP COLUMN "charging_method";
 ALTER TABLE "recurring_payments" DROP CONSTRAINT IF EXISTS "recurring_payments_account_uuid_foreign_id_service_id";
 COMMIT;
--- db/migrations/000004_alter_recurring_payments_add_column_and_constraint.up.sql
+++ db/migrations/000004_alter_recurring_payments_add_column_and_constraint.up.sql
@@ -1,5 +1,6 @@
 BEGIN;
 ALTER TABLE "recurring_payments" ADD COLUMN "foreign_id" varchar(255) NOT NULL;
+ALTER TABLE "recurring_payments" ADD COLUMN "charging_method" smallint NOT NULL;
 ALTER TABLE "recurring_payments" 
   ADD CONSTRAINT "recurring_payments_account_uuid_foreign_id_service_id" 
   UNIQUE ("account_uuid", "foreign_id", "service_id");
