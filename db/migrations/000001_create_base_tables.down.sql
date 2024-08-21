0a4b10a97ea122f15421be62ea1d63ff738bfa1f
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-08-21 10:03:08 +0800

    mig(1)!: fix down migration

--- db/migrations/000001_create_base_tables.down.sql
+++ db/migrations/000001_create_base_tables.down.sql
@@ -1,19 +1,11 @@
 BEGIN;
+ALTER TABLE "recurring_payments" DROP CONSTRAINT "recurring_payments_account_uuid_fkey";
+ALTER TABLE "transaction_histories" DROP CONSTRAINT "transaction_histories_dest_uuid_fkey";
+ALTER TABLE "transaction_histories" DROP CONSTRAINT "transaction_histories_account_uuid_fkey";
+ALTER TABLE "payment_accounts" DROP CONSTRAINT "payment_accounts_user_uuid_fkey";
 
-DROP INDEX IF EXISTS "idx_recurring_payments_scheduler_type";
-DROP INDEX IF EXISTS "idx_recurring_payments_account_uuid";
-DROP INDEX IF EXISTS "idx_recurring_payments_service_id";
-
-DROP INDEX IF EXISTS "idx_transaction_histories_status";
-DROP INDEX IF EXISTS "idx_transaction_histories_user_uuid";
-DROP INDEX IF EXISTS "idx_transaction_histories_service_id";
-
-DROP INDEX IF EXISTS "idx_payment_accounts_user_uuid";
-DROP INDEX IF EXISTS "idx_payment_accounts_service_id";
-
-DROP TABLE IF EXISTS "recurring_payments" CASCADE;
-DROP TABLE IF EXISTS "transaction_histories" CASCADE;
-DROP TABLE IF EXISTS "payment_accounts" CASCADE;
-DROP TABLE IF EXISTS "users" CASCADE;
-
+DROP TABLE IF EXISTS "recurring_payments";
+DROP TABLE IF EXISTS "transaction_histories";
+DROP TABLE IF EXISTS "payment_accounts";
+DROP TABLE IF EXISTS "users";
 COMMIT;
