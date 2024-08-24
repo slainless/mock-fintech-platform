6066a96d1cf7d9fae04232d31217548fd6ea0fe7
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-08-24 08:33:19 +0800

    mig(6)!: 🤦‍♂️ missing foreign key!

--- db/migrations/000006_added_shared_account_access.up.sql
+++ db/migrations/000006_added_shared_account_access.up.sql
@@ -6,6 +6,10 @@ CREATE TABLE "shared_account_access" (
   "permission" int NOT NULL,
   PRIMARY KEY ("account_uuid", "user_uuid")
 );
+
+ALTER TABLE "shared_account_access" ADD FOREIGN KEY ("account_uuid") REFERENCES "payment_accounts" ("uuid");
+ALTER TABLE "shared_account_access" ADD FOREIGN KEY ("user_uuid") REFERENCES "users" ("uuid");
+
 ALTER TABLE "transaction_histories" ADD COLUMN "issuer_uuid" uuid;
 ALTER TABLE "transaction_histories" ADD FOREIGN KEY ("issuer_uuid") REFERENCES "users" ("uuid");
 UPDATE "transaction_histories" SET "issuer_uuid" = (SELECT "user_uuid" FROM "payment_accounts" WHERE "payment_accounts"."uuid" = "transaction_histories"."account_uuid");
